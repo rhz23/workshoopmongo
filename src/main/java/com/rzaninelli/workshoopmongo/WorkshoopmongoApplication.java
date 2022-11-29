acfe788c9b0ba4c7727f3ef4e01cc99e3b7b3d84
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-11-29 19:47:19 -0300

    Initial commit

---
+++ src/main/java/com/rzaninelli/workshoopmongo/WorkshoopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.rzaninelli.workshoopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rzaninelli/workshoopmongo/WorkshoopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzaninelli.workshoopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
