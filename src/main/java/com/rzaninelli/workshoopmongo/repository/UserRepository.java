f8a37a4f27b78d5bf889925fe59b9111e56c246f
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-12-05 18:25:21 -0300

    Adicionado mongodb e estruturas basicas MVC spring

---
+++ src/main/java/com/rzaninelli/workshoopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rzaninelli.workshoopmongo.repository;
+
+import com.rzaninelli.workshoopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/rzaninelli/workshoopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.rzaninelli.workshoopmongo.services;
+
+import com.rzaninelli.workshoopmongo.domain.User;
+import com.rzaninelli.workshoopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
