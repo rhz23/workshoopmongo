cfa5e5fc110b011bcde20b0587f03122155b30e9
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-12-05 19:05:52 -0300

    Adicionado tratamento de exceção para requisições get errada

--- src/main/java/com/rzaninelli/workshoopmongo/resources/UserResource.java
+++ src/main/java/com/rzaninelli/workshoopmongo/resources/UserResource.java
@@ -1,9 +1,11 @@
 package com.rzaninelli.workshoopmongo.resources;
 
 import com.rzaninelli.workshoopmongo.domain.User;
+import com.rzaninelli.workshoopmongo.dto.UserDTO;
 import com.rzaninelli.workshoopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +23,17 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
 
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = userService.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/rzaninelli/workshoopmongo/services/UserService.java
+++ src/main/java/com/rzaninelli/workshoopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.rzaninelli.workshoopmongo.services;
 
 import com.rzaninelli.workshoopmongo.domain.User;
 import com.rzaninelli.workshoopmongo.repository.UserRepository;
+import com.rzaninelli.workshoopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+
+        return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
