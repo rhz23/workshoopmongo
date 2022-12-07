2e0e1525894fbf0b70ad5e33aa1a90915e33cbab
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-12-07 10:55:45 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/com/rzaninelli/workshoopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.rzaninelli.workshoopmongo.repository;
+
+import com.rzaninelli.workshoopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
